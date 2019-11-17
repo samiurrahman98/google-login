0be914827b723c3b839d623f9b6207eb3e076714
SAMIUR RAHMAN <S72RAHMA@uwaterloo.ca>
2019-11-17 16:58:14 -0500

    init, endpoint setup pending

---
+++ app.py
@@ -0,0 +1,45 @@
+# Python standard libraries
+import json
+import os
+import sqlite3
+
+# Third-party libraries
+from flask import Flask, redirect, request, url_for
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+# Internal imports
+from db import init_db_command
+from user import User
+
+# Config
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+
+# Flask app setup
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Naive database setup
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # already created
+    pass
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+# Flask-Login helper to retrieve user from db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+# ENDPOINTS PENDING
